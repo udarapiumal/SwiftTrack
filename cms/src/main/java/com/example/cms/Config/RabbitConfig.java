3a86e7db0e3ed173dcd0d1b777d948baf08b552a
udarapiumal <udarapiumal0@gmail.com>
2025-08-22 22:30:53 +0530

    Add

--- cms/src/main/java/com/example/cms/Config/RabbitConfig.java
+++ cms/src/main/java/com/example/cms/Config/RabbitConfig.java
@@ -6,10 +6,29 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
+
     public static final String ORDER_CREATED_QUEUE = "order.created";
+    public static final String ORDER_UPDATED_QUEUE = "order.updated";
+    public static final String ORDER_CANCELED_QUEUE = "order.canceled";
+    public static final String PACKAGE_STATUS_UPDATED_QUEUE = "package.status.updated";
 
     @Bean
     public Queue orderCreatedQueue() {
         return new Queue(ORDER_CREATED_QUEUE, true);
+    }
+
+    @Bean
+    public Queue orderUpdatedQueue() {
+        return new Queue(ORDER_UPDATED_QUEUE, true);
+    }
+
+    @Bean
+    public Queue orderCanceledQueue() {
+        return new Queue(ORDER_CANCELED_QUEUE, true);
+    }
+
+    @Bean
+    public Queue packageStatusUpdatedQueue() {
+        return new Queue(PACKAGE_STATUS_UPDATED_QUEUE, true);
     }
 }
